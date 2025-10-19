d849bed3194c0f016cc2889e925d82efb13c94fd
aconsonni19 <aconsonni19@campus.unimib.it>
2025-10-19 22:15:33 +0200

    MOD: Corrected typos

--- code_examples/example4.c
+++ code_examples/example4.c
@@ -5,7 +5,7 @@ int main() {
     if (x >= 5 && x <= 7) {
         printf("Numero tra 5 e 7!\n"); // Percorso A
     } else {
-        printf("Numero minore di 5 o inferiore di 7! \n") // Percorso B
+        printf("Numero minore di 5 o maggiore di 7! \n"); // Percorso B
     }
     return 0;
 }
